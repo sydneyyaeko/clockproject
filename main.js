be2385082c5a628389c034315c4e4facededdcb9
Sydney Yaeko <sydneyrodriguera@gmail.com>
2017-02-10 18:14:40 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function startTime() {
+//retrieving the time for today
+    var today = new Date();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+    minute = checkTime(minute);
+    second = checkTime(second);
+    document.getElementById('txt').innerHTML =
+
+ //summon the time and display with colons between the digits
+    hour + ":" + minute + ":" + second;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
